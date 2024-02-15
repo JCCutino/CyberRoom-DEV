b2b43e54fd95eda4f8b2758d7ae93069766aeee1
M-nogales <manuelnogalesserrano5221@gmail.com>
2024-02-15 16:33:52 +0100

    links updated + files moved

--- www/garage0.php
+++ www/garage0.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/garage.css">
+    <link rel="stylesheet" type="text/css" href="../resources/css/garage.css">
     <title>garage</title>
 </head>
 <body>
-    <img src="resources/img/screen.png" alt="garage screen" class="garage_screen">
+    <img src="../resources/img/screen.png" alt="garage screen" class="garage_screen">
     <div class="transparent-text">Texto aquí</div>
     <button class="next" href="" >Siguiente</button>
 
--- www/lintern.php
+++ www/lintern.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="resources/img/lichcol.png">
+    <link rel="icon" type="image/x-icon" href="../resources/img/lichcol.png">
 
     <title>Encuentra al caracol</title>
 
@@ -13,15 +13,13 @@
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
-        <link rel="stylesheet" type="text/css" href="css/lintern.css">
-        <script defer src="js/lintern.js"></script>
+        <link rel="stylesheet" type="text/css" href="../resources/css/lintern.css">
+        <script defer src="../resources/js/lintern.js"></script>
 </head>
 
 <body class="bg">
-    <!-- <img src="resources/img/AdventureTime.jpg" alt="caracoles" class="bg"> -->
-
     <!-- Imagen que activa el modal -->
-     <img src="resources/img/lichcol.png" class="clickable" alt="Descripción de la imagen"
+     <img src="../resources/img/lichcol.png" class="clickable" alt="Descripción de la imagen"
         data-bs-toggle="modal" data-bs-target="#exampleModal">
 
     <!-- Modal de Bootstrap -->
@@ -34,7 +32,7 @@
                 </div>
                 <div class="modal-body pb-0 bg-danger">
                     <h5> Parece que La Pupa estaba disfrazada del caracól de Hora de Aventuras </h5>
-                    <img src="resources/img/pupa2.png" class="img-fluid" alt="la puta sangrienta">
+                    <img src="../resources/img/pupa2.png" class="img-fluid" alt="la puta sangrienta">
                 </div>
                 <div class="modal-footer mt-0">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
