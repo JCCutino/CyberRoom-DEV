e4cd921cee63558e977fb0ddbaf95907154567c9
Luis Yerga <lyergamayor1@gmail.com>
2024-02-15 12:45:16 +0100

    img badulake y cosas

---
+++ resources/assets/apu.js
@@ -0,0 +1,7 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('apu').addEventListener('click', function () {
+        document.getElementById('enmascarado2').classList.toggle('active');
+    });
+});
