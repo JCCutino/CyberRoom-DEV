0057f6ca225251c9977a09303da8096b7c397550
Luis Yerga <lyergamayor1@gmail.com>
2024-02-16 12:31:21 +0100

    apu

---
+++ www/garage4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../resources/css/garage.css">
+    <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script defer src="../resources/js/garage4.js"></script>
+    <title>Garage 3</title>
+</head>
+<body>
+    <img src="../resources/img/screen.png" alt="garage screen" class="garage_screen">
+    <img src="../resources/img/pupa_sentada.png" class="pupa_sentada">
+    <img src="../resources/img/rick3.png" class="rick">
+    <img src="../resources/img/rick3.png" class="apu">
+    <div class="transparent-text"><p id="text">Quinta Misión</p></div>
+    <button class="next"  id="next" href="">Siguiente</button>
+
+</body>
+</html>
